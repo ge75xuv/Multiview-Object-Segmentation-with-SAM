a16d6ffef8f9f416ad0b416a9cd27288498428cc
tuna <ge75xuv@mytum.de>
2025-03-15 19:04:30 +0100

    100 sampled dataset for overtraining

--- custom_models/dataset/mini_dataset.py
+++ custom_models/dataset/mini_dataset.py
@@ -127,47 +127,12 @@ class MiniDataset(Dataset):
                     if (idx % len_video == len_video - 1) or (idx == len(timestamps)-1):
                         self.images.append(video_batch_image)
                         self.segmentation_masks.append(video_batch_seg_mask)
-
-        if False:
-            # Create paths to data directories and data path containers
-            data_paths = [root_path / ii for ii in split_folder_names]
-            seg_data = []
-            im_data = []
-            frame_indices = []
-
-            # Iterate the data take dirs 001_PKA, 003_TKA etc.
-            for dpath in data_paths:
-                # I dont understand somehow it is sim something 
-                if dpath == root_path / '005_TKA':
-                    continue
-                print(f'Currently extracting from folder: {dpath}\n')
-                # Get segmentation folders and camera indeces
-                seg_export_data_list = [dpath / dir_idx for dir_idx in os.listdir(dpath) if dir_idx.startswith('segmentation_export_')]  # Like [/.../segmetation_export_1, /.../segmetation_export_4]
-                camera_indices = [seg_exp_idx.name[-1] for seg_exp_idx in seg_export_data_list]
-
-                # Segemntation dictionary has the keys as take indices and value as the data path
-                # seg_dict = [{png_idx.split('.')[0].split('-')[1].split('-')[0]: seg_exp_idx / png_idx for png_idx in os.listdir(seg_exp_idx) if png_idx.startswith('camera')} for seg_exp_idx in seg_export_data_list]
-                seg_data_sub = [[ seg_exp_idx / png_idx for png_idx in os.listdir(seg_exp_idx) if png_idx.startswith('camera')] for seg_exp_idx in seg_export_data_list]
-                frame_indices_sub = [[re.findall(r'\d+', png_idx)[1] for png_idx in os.listdir(seg_exp_idx) if png_idx.startswith('camera')] for seg_exp_idx in seg_export_data_list]
-
-                # Print Log
-                num_data_in_take = sum([len(ii) for ii in frame_indices_sub])
-                print(f'{num_data_in_take} samples of segmentation data extracted from the folder: {dpath}\n')
-
-                # Set color image folder
-                color_image_path = dpath / "colorimage"
-                im_data_sub = [[color_image_path / f'camera0{camera_indices[ii]}_colorimage-{idx}.jpg' for idx in frame_idx] for ii, frame_idx in enumerate(frame_indices_sub)]
-
-                # Merge into main data storage
-                seg_data.extend(seg_data_sub)
-                frame_indices.extend(frame_indices_sub)
-                im_data.extend(im_data_sub)
-            print('Loading of the data path completed!')
-
-            # Store the data as self and as a whole list not list of lists
-            self.segmentation_masks = [x for xb in seg_data for x in xb]
-            self.images = [x for xb in im_data for x in xb]
-            assert len(self.segmentation_masks) == len(self.images), "There is a problem with the lengths of the data containers"
+        if split_type == 'over_train':
+            assert split_folder_names == ['001_PKA'], 'Only defined for 001_PKA'
+            assert object_labels == [10]
+            idx_range = range(2560, 2662)
+            self.images = [self.images[i] for i in idx_range]
+            self.segmentation_masks = [self.segmentation_masks[i] for i in idx_range]
 
     def __len__(self):
         return len(self.segmentation_masks)
