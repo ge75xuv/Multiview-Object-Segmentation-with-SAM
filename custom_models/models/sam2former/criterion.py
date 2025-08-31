Epipolar BUG fixed and retrain started + scores are included for gradient flow

custom_models/models/sam2former/criterion.py
@@ -18,7 +18,7 @@ from custom_models.helpers.configurations import OBJECT_FREQUENCY_PATH
 from .utils.misc import is_dist_avail_and_initialized, nested_tensor_from_tensor_list
 
 
-def sigmoid_focal_loss(inputs, targets, num_masks:float, alpha: float=0.25, gamma: float=2, alpha_vec: torch.Tensor=None, red_sum: bool=True):
+def sigmoid_focal_loss(inputs, targets, num_masks:float, alpha: float=0.25, gamma: float=2, red_sum: bool=True):
     """
     Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
     Args:
@@ -39,17 +39,18 @@ def sigmoid_focal_loss(inputs, targets, num_masks:float, alpha: float=0.25, gamm
     p_t = prob * targets + (1 - prob) * (1 - targets)
     loss = ce_loss * ((1 - p_t) ** gamma)
 
-    if alpha_vec is not None:
-        # alpha_vec is a vector of weights for each mask, used to weight the loss for each mask
-        alpha_t = alpha_vec * targets + (1 - alpha_vec) * (1 - targets)
+    if alpha >= 0:
+        alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
         loss = alpha_t * loss
-    elif alpha >= 0:
+    if isinstance(alpha, torch.Tensor):
+        # expects shape [C], broadcast across batch & spatial dims
+        # reshape to [1, C, 1, 1,...] for broadcasting
+        view_shape = [1, -1] + [1] * (loss.ndim - 2)
+        alpha = alpha.view(*view_shape).to(loss.device, loss.dtype)
         alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
         loss = alpha_t * loss
 
-    if red_sum:
-        return loss.mean(1).sum() / num_masks
-    return loss.mean(1)
+    return loss.mean(1).sum() / num_masks
 
 sigmoid_focal_loss_jit = torch.jit.script(
     sigmoid_focal_loss
@@ -167,7 +168,8 @@ class SetCriterion(nn.Module):
 
     def __init__(self, num_classes, matcher, weight_dict, eos_coef, losses,
                  num_points, oversample_ratio, importance_sample_ratio, image_size=512,
-                 loss_weighting='default', weight_soft_co=0.1, deep_supervision=True, pointwise_mask=True, obj_ids_extra_sampling=None):
+                 loss_weighting='default', weight_soft_co=0.1, deep_supervision=True, pointwise_mask=True, obj_ids_extra_sampling=None,
+                 alpha=0.2):
         """Create the criterion.
         Parameters:
             num_classes: number of object categories, omitting the special no-object category
@@ -184,6 +186,7 @@ class SetCriterion(nn.Module):
         self.losses = losses
         empty_weight = torch.ones(self.num_classes + 1)
         empty_weight[-1] = self.eos_coef
+        self.alpha = alpha
         self.weight_soft_co = weight_soft_co
         self.obj_ids_extra_sampling = obj_ids_extra_sampling if obj_ids_extra_sampling is not None else [2, 12, 13, 14]
 
@@ -287,11 +290,10 @@ class SetCriterion(nn.Module):
             # target_masks_down = self.resize_target_(target_masks).flatten(1)
             target_masks = target_masks.flatten(1)
             src_masks = src_masks.flatten(1)
-            alpha = 0.25
             # alpha_vec = self.empty_weight[tgt_idx[1]].to(src_masks.device)[:,None]
             losses = {
             # "loss_mask": sigmoid_ce_loss_jit(src_masks, target_masks_down, num_masks),
-            "loss_mask": sigmoid_focal_loss_jit(src_masks, target_masks, num_masks, alpha=alpha),
+            "loss_mask": sigmoid_focal_loss_jit(src_masks, target_masks, num_masks, alpha=self.alpha),
             "loss_dice": dice_loss_jit(src_masks, target_masks, num_masks),
         }
             del src_masks
@@ -413,7 +415,8 @@ class SetCriterion(nn.Module):
         outputs_without_aux = {k: v for k, v in outputs.items() if k != "aux_outputs"}
 
         # Treat absent objects as no obj
-        targets_filtered = [filter_absent_targets(t) for t in targets]
+        # targets_filtered = [filter_absent_targets(t) for t in targets]  # 18.08.
+        targets_filtered = targets
 
         # Retrieve the matching between the outputs of the last layer and the targets
         # indices are batch sized lists, where the first tensor is the indices of the outputs,

custom_models/models/sam2former/epipolar_maskv2.py
@@ -109,7 +109,8 @@ def single_epiline(_size: Tuple[int, int], lines: torch.Tensor, step: int = 10, 
     pts = torch.stack([x_valid, y_valid], dim=1)
     return pts, scores_valid
 
-def random_sample_mask_points(mask: torch.Tensor, n_points: int, threshold: float = 0.5) -> Optional[torch.Tensor]:
+def random_sample_mask_points(mask: torch.Tensor, n_points: int, 
+                              threshold: float = 0.5, depth_object: Optional[bool] = False) -> Optional[torch.Tensor]:
     # Handle multi-channel masks by summing across channels
     score_mask = mask.sigmoid()
     binary_mask = torch.sum(score_mask > threshold, dim=0)
@@ -121,8 +122,8 @@ def random_sample_mask_points(mask: torch.Tensor, n_points: int, threshold: floa
         return None, None
 
     # If we have fewer points than requested, return all available points
-    if len(x) <= n_points:
-        return torch.cat([x.unsqueeze(1), y.unsqueeze(1)], dim=1), score_mask[0, y, x]
+    if len(x) <= n_points or depth_object:
+        return torch.cat([x.unsqueeze(1), y.unsqueeze(1)], dim=1), score_mask[:, y, x].sum(0)
 
     # Randomly sample n_points indices
     indices = torch.randperm(len(x))[:n_points]
@@ -130,25 +131,21 @@ def random_sample_mask_points(mask: torch.Tensor, n_points: int, threshold: floa
     sampled_y = y[indices]
     coords = torch.cat([sampled_x.unsqueeze(1), sampled_y.unsqueeze(1)], dim=1)
 
-    return coords, score_mask[0, sampled_y, sampled_x]
+    return coords, score_mask[:, sampled_y, sampled_x].sum(0)
 
-@torch.no_grad()
+# @torch.no_grad()
 def epipolar_mask_preprocess(
     mask_cam0: torch.Tensor,
     mask_cam1: torch.Tensor,
     mask_cam2: torch.Tensor,
-    n_points: Optional[int] = 4096,
-    ):
+    n_points: Optional[int] = 1024,
+    depth_object: Optional[bool] = False
+):
     # Another reason for us to use temporality. It is not possible to make a differentiable masking here
     # The batch operation for different objects cannot be done here since the size of the masks are going to be varying size
-    pts_cam0, score0 = random_sample_mask_points(mask_cam0, n_points=n_points, threshold=0.5)
-    pts_cam1, score1 = random_sample_mask_points(mask_cam1, n_points=n_points, threshold=0.5)
-    pts_cam2, score2 = random_sample_mask_points(mask_cam2, n_points=n_points, threshold=0.5)
-
-    # Scores
-    # score0 = torch.sigmoid(mask_cam0[ind0]) if ind0 is not None else None
-    # score1 = torch.sigmoid(mask_cam1[ind1]) if ind1 is not None else None
-    # score2 = torch.sigmoid(mask_cam2[ind2]) if ind2 is not None else None
+    pts_cam0, score0 = random_sample_mask_points(mask_cam0, n_points=n_points, threshold=0.5, depth_object=depth_object)
+    pts_cam1, score1 = random_sample_mask_points(mask_cam1, n_points=n_points, threshold=0.5, depth_object=depth_object)
+    pts_cam2, score2 = random_sample_mask_points(mask_cam2, n_points=n_points, threshold=0.5, depth_object=depth_object)
     return pts_cam0, pts_cam1, pts_cam2, score0, score1, score2
 
 def epipolar_mask_post_process(
@@ -165,7 +162,8 @@ def epipolar_mask_post_process(
         weights = [epipolar_weight[v] for v in other_views]
         for i in range(2):  # two sets of epipolar intersections
             coords = epi_masks_for_views[view_id][i]  # [N, 2]
-            scores = score_masks_for_views[view_id][i] if score_masks_for_views is not None else 1
+            scores = score_masks_for_views[view_id][i]
+            scores = 1 if scores is None else scores
             if coords.numel() == 0:
                 continue
 
@@ -175,7 +173,7 @@ def epipolar_mask_post_process(
 
     return mask
 
-@torch.no_grad()
+# @torch.no_grad()
 def epipolar_mask(
     camera_int_ext: List[torch.Tensor],
     pts_cam0: torch.Tensor,
@@ -185,7 +183,6 @@ def epipolar_mask(
     step: int = 2,
     scores: List[torch.Tensor]=None,
     ):
-
     # Unpack camera intrinsics and extrinsics
     K0, ext0 = camera_int_ext[0]
     K1, ext1 = camera_int_ext[1]
@@ -204,6 +201,9 @@ def epipolar_mask(
 
     ext_0_to_2 = inv2 @ ext0
     ext_1_to_2 = inv2 @ ext1
+    
+    # Scores
+    scores = (None, None, None) if scores is None else scores
 
     # Compute epipolar lines
     epi_lines_1_to_0 = compute_epipolar_lines(ext_1_to_0, K0, K1, pts_cam1) if pts_cam1 is not None else None
@@ -233,12 +233,13 @@ def epipolar_mask(
         # Discriminate between the cases If both are None, skip this view
         if _check_first == False and _check_second == False:
             epi_masks_for_views[view] = torch.tensor([[]]), torch.tensor([[]])
+            score_masks_for_views[view] = torch.tensor([[]]), torch.tensor([[]])
             continue
 
         # If both lines exist, compute the intersection
         elif _check_first == True and _check_second == True:
-            intersect_point1, s1 = single_epiline(_size, _first_epi_lines, step, weights[0])
-            intersect_point2, s2 = single_epiline(_size, _second_epi_lines, step, weights[1])
+            intersect_point1, s1 = single_epiline(_size, _first_epi_lines, step=1, scores=weights[0])
+            intersect_point2, s2 = single_epiline(_size, _second_epi_lines, step=1, scores=weights[1])
             epi_masks_for_views[view] = intersect_point1, intersect_point2
             score_masks_for_views[view] = s1, s2
 
@@ -285,7 +286,7 @@ def epipolar_mask(
             raise RuntimeError("Unexpected case")
     return epi_masks_for_views, score_masks_for_views
 
-@torch.no_grad()
+# @torch.no_grad()
 def epipolar_main(camera_int_ext: List[torch.Tensor],
                   predictions0: Dict[str, torch.Tensor],
                   predictions1: Dict[str, torch.Tensor],
@@ -348,7 +349,7 @@ def epipolar_main(camera_int_ext: List[torch.Tensor],
         mask_cam2 = predictions2['pred_masks_high_res'][pos_obj2]  # instances of the same object.
 
         # Preprocess the masks to get points
-        pts_cam0, pts_cam1, pts_cam2, s0, s1, s2 = epipolar_mask_preprocess(mask_cam0, mask_cam1, mask_cam2, n_points=n_points)
+        pts_cam0, pts_cam1, pts_cam2, s0, s1, s2 = epipolar_mask_preprocess(mask_cam0, mask_cam1, mask_cam2, n_points=n_points, depth_object=depth_object)
 
         # Technically we dont need to check if the points are None, since the epipolar mask function will handle it
         # But we do it here to avoid unnecessary computation if the points are not available
@@ -358,18 +359,21 @@ def epipolar_main(camera_int_ext: List[torch.Tensor],
 
         if depth_object:
             # Point cloud projection wrapper
-            epi_mask_for_views = point_cloud_mask(camera_int_ext, pts_cam0, pts_cam1, pts_cam2, depth_images, mask_cam0.shape[-2:])
+            epi_mask_for_views, score_masks_for_views = point_cloud_mask(camera_int_ext, pts_cam0, pts_cam1, pts_cam2, depth_images, mask_cam0.shape[-2:], scores=[s0, s1, s2])
         else:
             # Run the epipolar mask function
             epi_mask_for_views, score_masks_for_views = epipolar_mask(camera_int_ext, pts_cam0, pts_cam1, pts_cam2, 
                                                mask_cam0.shape[-2:], step=step, scores=[s0, s1, s2])
+
+        # Assert
+        assert len(epi_mask_for_views) == 3, "Epipolar mask for views should have 3 entries"
+        assert len(score_masks_for_views) == 3, "Score mask for views should have 3 entries"
 
         # Find the prob of that object
         prob_obj0 = pred_logits0[pos_obj0].max()  if pos_obj0[0].numel() > 0 else 0.0
         prob_obj1 = pred_logits1[pos_obj1].max()  if pos_obj1[0].numel() > 0 else 0.0
         prob_obj2 = pred_logits2[pos_obj2].max()  if pos_obj2[0].numel() > 0 else 0.0
 
-        score_masks_for_views = None  # TODO make it work after
         # Epipolar mask post processing
         mask = epipolar_mask_post_process(epi_mask_for_views, mask_cam0.shape[-2:], 
                                           [prob_obj0, prob_obj1, prob_obj2], score_masks_for_views, device)

custom_models/models/sam2former/point_cloud_mask.py
@@ -30,7 +30,7 @@ def backproject_to_world(pts_cam, K, T, mask_depth):
     obj_pc = T @ torch.concatenate([im_coords_depth_scaled, torch.ones_like(x_)[None,:]], axis=0)  # Transforming to world coordinates 4xN
     return obj_pc
 
-def reprojection_to_image_frame(K, T, obj_pc, H, W):
+def reprojection_to_image_frame(K, T, obj_pc, H, W, score=None):
     # Projecting back to image coordinates
     projected_pts = K @ (torch.linalg.inv(T) @ obj_pc)[:3, :]  # 3xN
     normalized_pts = projected_pts / projected_pts[2, :]
@@ -40,12 +40,16 @@ def reprojection_to_image_frame(K, T, obj_pc, H, W):
     # Refinement
     refinement = (pixel_pts[1] > (H-1)) | (pixel_pts[1] < 0) | (pixel_pts[0] > (W-1)) | (pixel_pts[0] < 0)
     pixel_pts[:, refinement] = 0  # Set invalid pixel coordinates to 0
-    return pixel_pts
+    if score is not None:
+        score[refinement] = 0  # Set scores of invalid pixel coordinates to 0
+    return pixel_pts, score
 
-def point_cloud_mask(cam_int_ext, pts_cam0, pts_cam1, pts_cam2, gt_depth_image, img_size):
+def point_cloud_mask(cam_int_ext, pts_cam0, pts_cam1, pts_cam2, gt_depth_image, img_size, scores=None):
     # Store Intrinsics and extrinsics
     K0, K1, K2 = cam_int_ext[0][0], cam_int_ext[1][0], cam_int_ext[2][0]
     T0, T1, T2 = cam_int_ext[0][1], cam_int_ext[1][1], cam_int_ext[2][1]  # Transformations are from camera to world coordinates
+
+    s0, s1, s2 = scores if scores is not None else (None, None, None)
 
     # Create empty point clouds
     device = cam_int_ext[0][0].device
@@ -73,19 +77,25 @@ def point_cloud_mask(cam_int_ext, pts_cam0, pts_cam1, pts_cam2, gt_depth_image, 
     # Reprojection to the image frame
     H, W = img_size
     # To 0
-    pixel_pts0_1 = reprojection_to_image_frame(K0, T0, obj_pc1, H, W) if obj_pc1.shape[1] > 0 else torch.tensor([[]])
-    pixel_pts0_2 = reprojection_to_image_frame(K0, T0, obj_pc2, H, W) if obj_pc2.shape[1] > 0 else torch.tensor([[]])
+    pixel_pts0_1, score0_1 = reprojection_to_image_frame(K0, T0, obj_pc1, H, W, s1) if obj_pc1.shape[1] > 0 else (torch.tensor([[]]), None)
+    pixel_pts0_2, score0_2 = reprojection_to_image_frame(K0, T0, obj_pc2, H, W, s2) if obj_pc2.shape[1] > 0 else (torch.tensor([[]]), None)
     # To 1
-    pixel_pts1_0 = reprojection_to_image_frame(K1, T1, obj_pc0, H, W) if obj_pc0.shape[1] > 0 else torch.tensor([[]])
-    pixel_pts1_2 = reprojection_to_image_frame(K1, T1, obj_pc2, H, W) if obj_pc2.shape[1] > 0 else torch.tensor([[]])
+    pixel_pts1_0, score1_0 = reprojection_to_image_frame(K1, T1, obj_pc0, H, W, s0) if obj_pc0.shape[1] > 0 else (torch.tensor([[]]), None)
+    pixel_pts1_2, score1_2 = reprojection_to_image_frame(K1, T1, obj_pc2, H, W, s2) if obj_pc2.shape[1] > 0 else (torch.tensor([[]]), None)
     # To 2
-    pixel_pts2_0 = reprojection_to_image_frame(K2, T2, obj_pc0, H, W) if obj_pc0.shape[1] > 0 else torch.tensor([[]])
-    pixel_pts2_1 = reprojection_to_image_frame(K2, T2, obj_pc1, H, W) if obj_pc1.shape[1] > 0 else torch.tensor([[]])
+    pixel_pts2_0, score2_0 = reprojection_to_image_frame(K2, T2, obj_pc0, H, W, s0) if obj_pc0.shape[1] > 0 else (torch.tensor([[]]), None)
+    pixel_pts2_1, score2_1 = reprojection_to_image_frame(K2, T2, obj_pc1, H, W, s1) if obj_pc1.shape[1] > 0 else (torch.tensor([[]]), None)
 
     pc_masks_for_views = {
         0: (pixel_pts0_1.T, pixel_pts0_2.T),
         1: (pixel_pts1_0.T, pixel_pts1_2.T),
         2: (pixel_pts2_0.T, pixel_pts2_1.T),
     }
+    
+    score_masks_for_views = {
+        0: (score0_1, score0_2),
+        1: (score1_0, score1_2),
+        2: (score2_0, score2_1),
+    }
 
-    return pc_masks_for_views
+    return pc_masks_for_views, score_masks_for_views

custom_models/models/sam2former/sam2former_base.py
@@ -15,7 +15,7 @@ from sam2.modeling.sam2_utils import get_1d_sine_pe, MLP, select_closest_cond_fr
 
 from .lib import ShapeSpec
 from .mask_former_head import MaskFormerHead
-from custom_models.helpers.configurations import OBJECTS_EPIPOLAR
+from custom_models.helpers.configurations import OBJECTS_EPIPOLAR, OBJECTS_DEPTH_PROJ
 
 # a large negative value as a placeholder score for missing objects
 NO_OBJ_SCORE = -1024.0
@@ -506,7 +506,8 @@ class SAM2FormerBase(torch.nn.Module):
         """Fuse the current frame's visual feature map with previous epipolar features."""
         # The shape is changed in the memory encoding, change it back to default.
         current_vision_feats = current_vision_feats.flatten(2).permute(2, 0, 1)
-        B = current_vision_feats.size(1)  # batch size on this frame
+        # B = current_vision_feats.size(1)  # batch size on this frame
+        B = 1  # Set it to 1 for single-frame processing
         C = self.hidden_dim
         H, W = feat_sizes[-1]  # top-level (lowest-resolution) feature size
         device = current_vision_feats[-1].device
@@ -537,9 +538,8 @@ class SAM2FormerBase(torch.nn.Module):
         epi_pos_enc = epi_pos_enc[:, ordered_obj_label]
 
         # Step 3: Add the no-epipolar embedding to the object that we dont calculate epipolars for
-        #NOTE THIS APPROACH IS NOT POSSIBLE SINCE THE EMBEDDING DIMENSIONS ARE NOT THE SAME (EPIPOLAR ENCODING CHANGES EMBEDDING DIMENSION)
         multi_obj_no_epipolar_embed = torch.zeros_like(epi_feats)
-        no_epipolar_mask = [pos for pos, lbl in object_pos_label.items() if lbl not in OBJECTS_EPIPOLAR]
+        no_epipolar_mask = [pos for pos, lbl in object_pos_label.items() if not (lbl in OBJECTS_EPIPOLAR or lbl in OBJECTS_DEPTH_PROJ)]
         no_epipolar_object_embed = self.no_epipolar_embed_proj(self.no_epipolar_embed)
         multi_obj_no_epipolar_embed[:, no_epipolar_mask] = no_epipolar_object_embed
         epi_feats += multi_obj_no_epipolar_embed
@@ -548,7 +548,7 @@ class SAM2FormerBase(torch.nn.Module):
         # epi_feats = self.multi_object_epi_proj(epi_feats.mT).mT
         epi_pos_enc = epi_pos_enc[:, 0:1]
         # epi_pos_enc = self.multi_object_epi_pos_proj(epi_pos_enc.mT).mT
-        
+
         if current_vision_feats.shape[1] != epi_feats.shape[1]:
             Q = epi_feats.shape[1]
             current_vision_feats = current_vision_feats.repeat(1, Q, 1)
@@ -763,7 +763,11 @@ class SAM2FormerBase(torch.nn.Module):
             num_obj_ptr_tokens=num_obj_ptr_tokens,
         )
         if not early_collapse:
-            pix_feat_with_mem = self.multi_object_memory_proj(pix_feat_with_mem.transpose(0,1)).transpose(0,1)
+            if self.multiview and not self.flag_epipolar_attn_bias:
+                pix_feat_with_mem = pix_feat_with_mem.permute(1, 2, 0).view(O, C, H, W)
+                return pix_feat_with_mem
+            else:
+                pix_feat_with_mem = self.multi_object_memory_proj(pix_feat_with_mem.transpose(0,1)).transpose(0,1)
 
         # reshape the output (HW)BC => BCHW
         pix_feat_with_mem = pix_feat_with_mem.permute(1, 2, 0).view(B, C, H, W)

custom_models/models/sam2former/sam2former_train.py
@@ -37,6 +37,7 @@ class SAM2FormerTrain(SAM2FormerBase):
         epipolar_encoder=None,
         epipolar_attention=None,
         query_epipolar_fusion=None,
+        run_first_no_grad=True,
         flag_epipolar_attn_bias=False,
         source_target_reliability=None,
         view_spatial_prior=None,
@@ -137,6 +138,7 @@ class SAM2FormerTrain(SAM2FormerBase):
             "Invalid configuration: `flag_epipolar_attn_bias` can only be True if `multiview` is also True.")
 
         if self.multiview:
+            self.run_first_no_grad = run_first_no_grad
             # Source-Target Pair Reliability
             self.source_target_reliability = source_target_reliability
             # ViewSpatialPrior
@@ -307,7 +309,7 @@ class SAM2FormerTrain(SAM2FormerBase):
                 #TODO Inject the Film Layer here, we need the view idx
 
                 # Get output masks based on this frame's prompts and previous memory. Depending on the last decoding or not, apply the appropriate context.
-                ctx = torch.no_grad() if self.multiview else nullcontext()
+                ctx = torch.no_grad() if self.multiview and self.run_first_no_grad else nullcontext()
                 with ctx:
                     current_out = self.track_step(
                             frame_idx=stage_id,
@@ -349,11 +351,10 @@ class SAM2FormerTrain(SAM2FormerBase):
                                                                  self.sam_mask_decoder.predictor.num_queries,
                                                                  depth_images=stage_depth_images)
                 epipolar_masks = epipolar_masks.to(self.device)
-                # SourceTargetReliability
-                epipolar_masks = self.source_target_reliability(epipolar_masks)
-
                 # If we are using the epipolars as attention bias
                 if self.flag_epipolar_attn_bias:
+                    # SourceTargetReliability
+                    epipolar_masks = self.source_target_reliability(epipolar_masks)
                     # ViewSpatialPrior
                     view_spatial_prior = self.view_spatial_prior([0,1,2], *epipolar_masks.shape[-2:])
                     # ReliabilityAndBias
@@ -385,14 +386,15 @@ class SAM2FormerTrain(SAM2FormerBase):
 
                 else:
                     # Scale the epipolar masks and add bias (Use the same scale and bias as in the memory encoder)
-                    pseudo_masks = epipolar_masks.sigmoid() * self.sigmoid_scale_for_mem_enc 
-                    pseudo_masks = pseudo_masks.sum(2) + self.sigmoid_bias_for_mem_enc
+                    pseudo_masks = epipolar_masks.sum(2)  # The scores are already after sigmoid (see. epipolar preprocess scores)
+                    pseudo_masks = pseudo_masks * self.sigmoid_scale_for_mem_enc + self.sigmoid_bias_for_mem_enc
                     pix_feat = torch.vstack(list(feature_container_for_multiview_fusion.values()))
                     # NOTE pix_feats = (3, 256, 16, 16), pseudo_masks = (O, 3, H, W)
                     # NOTE pix_feat_for_mem = (1, 256, 16, 16) mask_for_mem = (23, 1, 256, 256)
                     # Encode the epipolar features
                     for view_idx in range(3):
-                        encoded_epipolars = self.epipolar_encoder(pix_feat[view_idx:view_idx+1], pseudo_masks[:,view_idx:view_idx+1])
+                        encoded_epipolars = self.epipolar_encoder(pix_feat[view_idx:view_idx+1], pseudo_masks[:,view_idx:view_idx+1],
+                                                                  skip_mask_sigmoid=True)
                         # dictionary with keys "vision_features" and "vision_pos_enc"
                         encoded_epipolars['object_positions_labels'] = object_pos_label[view_idx]
                         encoded_epipolar_dict[view_idx] = encoded_epipolars